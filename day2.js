8f554bd85311d2677df4a90ed4baac932090eb89
Tony Ngan <tonynwk919@gmail.com>
2017-12-08 02:02:09 +0800

    Update day2.js

--- day2.js
+++ day2.js
@@ -1,3 +1,5 @@
 f=i=>i.split('\n').map(a=>Math.max(...a.split(' '))-Math.min(...a.split(' '))).reduce((a,b)=>a+b)
 
 // f(input)
+
+p=m=>m.map(a=>a.map(b=>a.map((c,i)=>c%b==0?a[i]:null).filter(s=>s!==null).sort((a,b)=>(+a)-(+b))).filter(c=>c.length==2).map(([d,e])=>e/d)).reduce((f,g)=>+f+(+g))
