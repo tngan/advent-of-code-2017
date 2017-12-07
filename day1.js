a6bdc696d831c20b83cfc9002797717d9aad555a
Tony Ngan <tonynwk919@gmail.com>
2017-12-08 00:18:30 +0800

    Update day1.js

--- day1.js
+++ day1.js
@@ -1,3 +1,15 @@
+// part1
+
 f=i=>[...i].reduce(([l,a],r)=>[r,a+(l==r?+l:0)],[+i[i.length-1],0])[1]
 
-// f(input)
+// part2
+
+p = i => {
+  j = i + i.slice(0, i.length/2)
+  a = 0
+  for(var k=0; k<i.length; k++) {
+    s = [...j].slice(k, k+1+i.length/2)
+    a += s[s.length - 1] == i[k] ? +i[k] : 0;
+  }
+  return a;
+}
