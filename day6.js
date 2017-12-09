54c8940f5c083f1f416a4f3b61e5d9dff7c9e5d0
Tony Ngan <tonynwk919@gmail.com>
2017-12-10 00:27:33 +0800

    Create day6.js

---
+++ day6.js
@@ -0,0 +1,28 @@
+// part1
+
+input = '11 11 13 7 0 15 5 5 4 4 1 1 7 1 15 11'.split(' ').map(Number).slice()
+
+memory = new Set();
+
+function distributor() {
+  const p = input.indexOf(Math.max(...input)); 
+  for(let i=input[p], j=0; i>=1; i--) {
+    pointer = (p+(j++)+1)%input.length;
+    input[p]--;
+    input[pointer] += 1;
+  }
+}
+
+function run() {
+  while(true) {
+    distributor();
+    if (!memory.has(String(input.map(String)))) {
+      memory.add(String(input.map(String)));
+    } else {
+      break;
+    }
+  }
+  console.log([...memory].length+1);
+}
+
+run();
